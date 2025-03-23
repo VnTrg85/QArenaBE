9e3e939f1312401301ee4817a884c59cd6d389ba
Tung Pham <tnpham2709@gmail.com>
2025-03-23 22:44:27 +0700

    update entity+

---
+++ src/main/java/qarenabe/qarenabe/entity/Message.java
@@ -0,0 +1,43 @@
+package qarenabe.qarenabe.entity;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "bug_report_Id")
+    @Getter
+    @Setter
+    private BugReport bugReport;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @Getter
+    @Setter
+    private User user;
+}
